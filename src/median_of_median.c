bffa24db1944ab9cbba5e69a285e18f299f07942
Ivan Radanov Ivanov <ivanradanov@yahoo.co.uk>
2018-07-03 02:29:52 +0900

    median of medians

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -9,23 +9,59 @@ int A[N];
 A[0], A[1], ..., A[n-1] の中でk+1番目に小さい値を返す関数
 ただし、Aの中身は書き換えてしまう。
 */
-int quick_select(int A[], int n, int k){
-  int i, j, pivot;
 
-// 先頭の要素をピボットとする
-  pivot = A[0];
-  for(i = j = 1; i < n; i++){
-    if(A[i] <= pivot){
-      int z = A[j];
-      A[j] = A[i];
-      A[i] = z;
-      j++;
-    }
-  }
+#define SWAP(a, b) do {int c = a; a = b; b = c;} while(0)
 
-  if(j == k+1) return pivot;
-  else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
+void pretty_print(int A[], int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%d\t", A[i]);
+	printf("\n");
+}
+
+int median(int A[], int n)
+{
+	for (int i = 1; i < n; i++)
+		for (int j = i; j > 0 && A[j] < A[j - 1]; j--)
+			SWAP(A[j], A[j-1]);
+	return A[n / 2];
+}
+
+int quick_select(int A[], int n, int k);
+
+int select_pivot(int A[], int n)
+{
+	if (n <= 5)
+		return median(A, n);
+	int B[N/5 + 1], i, j;
+	for (i = 0, j = 0; j < n; i++, j += 5)
+		if (j + 5 < n) {
+			B[i] = median(A + j, 5);
+		} else {
+			B[i] = median(A + j, n - j);
+			break;
+		}
+	int sn = i + 1;
+	return quick_select(B, sn, sn / 2);
+}
+
+int quick_select(int A[], int n, int p)
+{
+	int pivot = select_pivot(A, n), j = 0, k = 0;
+	for (int i = 0; i < n; i++) {
+		if (A[i] < pivot) {
+			SWAP(A[k], A[j]);
+			SWAP(A[i], A[j]);
+			k++;
+			j++;
+		} else if (A[i] == pivot) {
+			SWAP(A[i], A[k]);
+			k++;
+		}
+	}
+	if (j <= p && p < k) return pivot;
+	else if (p < j) return quick_select(A, j, p);
+	else return quick_select(A + k, n - k, p - k);
 }
 
 int main(){
